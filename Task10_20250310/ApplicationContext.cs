93b7ce852dd8c8c623eb04c05af6d172cc461f2c
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-10 16:05:42 -0600

    Practice data loading

---
+++ Task10_20250310/ApplicationContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10_20250310
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; } = null!;
+        public DbSet<Shop> Shops { get; set; } = null!;
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=MIRUAHUA;Initial Catalog=March_GameShop;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Shop>().Property(s => s.Name).IsRequired();
+            modelBuilder.Entity<Company>().Property(c => c.Name).IsRequired();
+            modelBuilder.Entity<Customer>().Property(c => c.Name).IsRequired();
+
+            modelBuilder.Entity<Shop>()
+            .HasOne(s => s.Company)  // Shop has one Company
+            .WithMany(c => c.Shops)  // Company has many Shops
+            .HasForeignKey(s => s.CompanyId)  // Foreign Key in Shops table
+            .OnDelete(DeleteBehavior.Cascade); // Cascade delete
+
+            modelBuilder.Entity<Customer>()
+                .HasMany(c => c.Shops)
+                .WithMany(s => s.Customers);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Task10_20250310/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Task10_20250310
+{
+    /*
+     Опишите 3 связанных таблицы: «Компания»,
+«Магазин» и «Покупатели». Каждый магазин должен принадлежать к
+определенной компании, каждый покупатель может принадлежать к нескольким
+магазинам. 
+ 
+Реализуйте возможность добавления и вывода информации, к примеру: 
+ 
+1) Отобразите информацию обо всех компаниях.
+ 
+Название компании, список магазинов (магазинов со своими покупателями может быть несколько), список покупателей для данного магазина.
+ 
+2) Отобразите информацию о покупателях.
+ 
+Фио пользователя, список магазинов и их компаний.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Shop> Shops { get; set; } // nav prop
+    }
+    public class Shop
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int CompanyId { get; set; } // foreign key
+        public Company Company { get; set; } //nav prop
+        public List<Customer> Customers { get; set; }
+    }
+
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Shop> Shops { get; set; } //nav prop
+    }
+}
