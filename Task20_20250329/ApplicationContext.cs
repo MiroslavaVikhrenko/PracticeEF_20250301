935b64f1ef32c89f3bb3ee5fb563d172873279c0
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-29 14:12:49 -0600

    Practice EF Core and SQL

---
+++ Task20_20250329/ApplicationContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task20_20250329
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Train> Trains { get; set; } = null!;
+        public DbSet<Station> Stations { get; set; } = null!;
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Data Source=MIRUAHUA;Initial Catalog=March_TrainStationSystem;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Set required props
+            modelBuilder.Entity<Station>().Property(s => s.Name).IsRequired();
+            modelBuilder.Entity<Train>().Property(t => t.Number).IsRequired();
+            modelBuilder.Entity<Train>().Property(t => t.Model).IsRequired();
+
+            // Station - Train relations
+            modelBuilder.Entity<Train>()
+                .HasOne(t => t.Station) // Each Train must have one Station
+                .WithMany(s => s.Trains) // A station can have many trains
+                .HasForeignKey(e => e.StationId) // Foreign key to Station
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Task20_20250329/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Task20_20250329
+{
+    internal class Program
+    {
+        /*
+         Создать таблицы: «Станция» и «Поезд». Используя метод FromSqlRaw и ExecuteSqlRaw, 
+        выполнить 8 запросов для получения данных:
+
+Добавить данные про станции и поезда.
+Поезда у которых длительность маршрута более 5 часов.
+Общую информация о станции и ее поездах.
+Название станций у которой в наличии более 3-ех поездов.
+Все поезда, модель которых начинается на подстроку «Pell».
+Все поезда, у которых возраст более 15 лет с текущей даты.
+Получить станции, у которых в наличии хотя бы один поезд с длительность маршрутка менее 4 часов.
+Вывести все станции без поездов (на которых не будет поездов при выполнении LEFT JOIN).
+         */
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                List<Station> stations = new List<Station>()
+                {
+                    new Station(){Name = "Calgary"},
+                    new Station(){Name = "Edmonton"}
+                };
+                db.Stations.AddRange(stations);
+                db.SaveChanges();
+
+                List<Train> trains = new List<Train>()
+                {
+                    new Train(){Number = "347", Model = "M1", TravelTime = new TimeSpan(5,30, 10), ManufacturingDate = new DateOnly(2017, 03, 01), StationId = 1},
+                    new Train(){Number = "348", Model = "M2", TravelTime = new TimeSpan(4,30, 10), ManufacturingDate = new DateOnly(2019, 03, 01), StationId = 1},
+                    new Train(){Number = "349", Model = "M1", TravelTime = new TimeSpan(2,30, 10), ManufacturingDate = new DateOnly(2021, 03, 01), StationId = 2},
+                    new Train(){Number = "350", Model = "M2", TravelTime = new TimeSpan(6,30, 10), ManufacturingDate = new DateOnly(2023, 03, 01), StationId = 2},
+                    new Train(){Number = "351", Model = "M1", TravelTime = new TimeSpan(1,30, 10), ManufacturingDate = new DateOnly(2015, 03, 01), StationId = 1},
+                };
+                db.Trains.AddRange(trains);
+                db.SaveChanges();
+
+                // Добавить данные про станции и поезда.
+            }
+        }
+    }
+
+    public class Station
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Train> Trains { get; set; }
+    }
+    public class Train
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public string Model { get; set; }
+        public TimeSpan TravelTime { get; set; }
+        public DateOnly ManufacturingDate { get; set; }
+
+        public int StationId { get; set; }
+        public Station Station { get; set; }
+    }
+}
